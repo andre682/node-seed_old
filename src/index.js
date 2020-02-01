b7a24654ee69a9f4cd26f5c98dfe93add7fb3fa2
Andre Torbitoni <andre.torbitoni@gmail.com>
2020-02-01 10:41:47 -0500

    Config express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+import 'dotenv/config';
+import express from 'express'
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}/`);
+  });
