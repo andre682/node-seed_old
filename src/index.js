c9e577e77b7f589aae6831e883003cd0646e61c8
Andre Torbitoni <andre.torbitoni@gmail.com>
2020-02-04 19:15:30 -0500

    prettier & eslint config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import 'dotenv/config';
-import express from 'express'
+import express from 'express';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-  });
+  res.send('Hello World');
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}/`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
